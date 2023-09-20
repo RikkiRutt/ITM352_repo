8cd6299b633538f19211c191a7da5aedc8eac5e8
RikkiRutt <rikkir@hawaii.edu>
2023-09-19 23:55:33 -1000

    L5

--- Lab5/lab5.js
+++ Lab5/lab5.js
@@ -1,4 +1,13 @@
+
+//declaring variables per instructions
 let age=33;
 let fave_num=96;
 let day_of_birth=9;
 let month_of_birth=5;
+
+/* do two calculations with same variables using
+ diffrent precedence to show how precedence works*/
+
+let calculations1 =age + fav_num / day_of_birth * month_of_birth; 
+let calculations2 =(age + fav_num) / day_of_birth * month_of_birth;
+
