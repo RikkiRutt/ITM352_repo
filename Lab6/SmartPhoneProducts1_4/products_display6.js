eeb585413dce970d7621c4a906653a5db542e13c
RikkiRutt <rikkir@hawaii.edu>
2023-10-01 23:37:10 -1000

    L6

--- Lab6/SmartPhoneProducts1_4/products_display6.js
+++ Lab6/SmartPhoneProducts1_4/products_display6.js
@@ -5,6 +5,7 @@ bottom_title.innerHTML=(store_name + "'s Used Smart Phone Store");
 
 let hits= 0;
 let spins=0;
+let wins;
 hits_span.innerHTML = hits; 
 spins_span.innerHTML = spins;
 
@@ -12,12 +13,22 @@ function changeClassName(element) {
   
     element.className = 'item rotate';
     spins=spins+1; 
+    if(spins<2*hits){
+        wins=true;
+    } else {
+        wins=false;
+    }
     spins_span.innerHTML = spins; 
     hit_spin_span.innerHTML=Number(hits/spins).toFixed(2)
 }
 function resetClassName(element) {
     element.className = 'item';
     hits=hits+=2;
+    if(spins<2*hits){
+        wins=true;
+    } else {
+        wins=false;
+    }
     hits_span.innerHTML = hits; 
     hit_spin_span.innerHTML=Number(hits/spins).toFixed(2)
 }
