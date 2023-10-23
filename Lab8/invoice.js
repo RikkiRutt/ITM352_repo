1aa16df1f89ebbf27b90d121cee1f6bd347b8ea5
RikkiRutt <rikkir@hawaii.edu>
2023-10-22 23:59:29 -1000

    L8

--- Lab8/invoice.js
+++ Lab8/invoice.js
@@ -2,7 +2,7 @@
 let item1 = 'Hat';
 let quantity1 = 1;
 let price1 = 25.26;
-let extended_price1 = quantity1 * price1;
+//let extended_price1 = quantity1 * price1;
 
 let item2 = 'Shirt';
 let quantity2 = 4;
@@ -27,9 +27,12 @@ let extended_price5 = quantity5 * price5;
 //lab 8 part 1.1
 let product1 = {
     itemName: 'iPhone12',
-    quantitu: 4,
+    quantity: 4,
     price: 247.95
 };
+
+//lab8 part 1.2
+let extended_price1 = product1.quantity * product1.price;
 
 //calculate subtotal
 let subtotal = extended_price1 + extended_price2 + extended_price3 + extended_price4 + extended_price5;
@@ -38,30 +41,16 @@ let subtotal = extended_price1 + extended_price2 + extended_price3 + extended_pr
 let taxRate = 0.0575;  // 5.75%
 let taxAmount = subtotal * taxRate;
 
-//calculate shipping
-let shippingCharge = 0
-if (subtotal <= 50)
-{
-    shippingCharge = 2
-} //lest then$50 is $2
-else if (subtotal <= 100) 
-{
-    shippingCharge = 5
-} //$50 to $99.99 is $5
-else
-{
-    shippingCharge= subtotal * 0.05 //5% of subtotal
-} //more then $100 is 5% of subtotal
 //calculate total
-let total = subtotal + taxAmount + shippingCharge;
+let total = subtotal + taxAmount;
 
 //populate the table rows using dom manipulation
 let table = document.getElementById('invoiceTable');
 
 let row = invoiceTable.insertRow(); //create new row for each item
-row.insertCell(0).innerHTML = `${item1}`; 
-row.insertCell(1).innerHTML = `${quantity1}`;
-row.insertCell(2).innerHTML = `$` + `${price1}`;
+row.insertCell(0).innerHTML = `${product1.itemName}`; 
+row.insertCell(1).innerHTML = `${qproduct1.quantity}`;
+row.insertCell(2).innerHTML = `$` + `${product1.price}`;
 row.insertCell(3).innerHTML = (`$` + `${extended_price1}`);
 row = invoiceTable.insertRow(); //create new row for each item
 row.insertCell(0).innerHTML = `${item2}`; 
