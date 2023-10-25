787d636ec0782cbd468e0a4e8faab2bc70bdef7e
RikkiRutt <rikkir@hawaii.edu>
2023-10-24 22:11:19 -1000

    L8

--- Lab8/lab8ex3.js
+++ Lab8/lab8ex3.js
@@ -1,3 +1,14 @@
 //define the product_quantities array
 let product_quantities = [2,1,1,1,1];
 
+// iterate through product_quantities and print each element in a table
+document.write("<table>");
+document.write("<tr><tr>Product #</th><th>Quantity</th></th>");
+
+for (let i=0; i < product_quantities.length; i++) {
+    document.write("<tr>");
+    document.write("<td>" + (i + 1) + "</td>"); // product #
+    document.write("<td>" + product_quantities[i] + "</td>"); // quantity
+    document.write("</tr>");
+}
+document.write("</table>");
