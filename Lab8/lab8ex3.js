a97de556250601d5c9ecf444847e8afa8de5c537
RikkiRutt <rikkir@hawaii.edu>
2023-10-24 22:12:55 -1000

    L8

--- Lab8/lab8ex3.js
+++ Lab8/lab8ex3.js
@@ -1,6 +1,17 @@
 //define the product_quantities array
 let product_quantities = [2,1,1,1,1];
 
+// array of all products
+// corresponds to product_quantities array
+// product_quantities[i] is the quantity for products[i]
+let products = [
+    { 'name': 'small gumball', 'price': 0.02 },
+    { 'name': 'medium gumball', 'price': 0.05 },
+    { 'name': 'large gumball', 'price': 0.07 },
+    { 'name': 'small jawbreaker', 'price': 0.06 },
+    { 'name': 'large jawbreaker', 'price': 0.10 }
+   ];
+
 // iterate through product_quantities and print each element in a table
 document.write("<table>");
 document.write("<tr><tr>Product #</th><th>Quantity</th></th>");
@@ -12,3 +23,4 @@ for (let i=0; i < product_quantities.length; i++) {
     document.write("</tr>");
 }
 document.write("</table>");
+
