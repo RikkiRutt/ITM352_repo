b4b550fec1e3fe1010b63ac37cb5b056866b75d7
RikkiRutt <rikkir@hawaii.edu>
2023-10-29 23:27:17 -1000

    L11

---
+++ Lab11/order5.js
@@ -0,0 +1,43 @@
+
+
+function formSubmission(){
+
+    let quantity = Number(document.querySelector('input[name="qty_textbox"]').value);
+
+let validationMessage = validateQuantity(quantity);
+
+if (validationMessage!=="") {
+    document.getElementById("invalidQuantity").innerHTML = validationMessage
+} else {
+
+    window.location.href = `display_purchase.html?qty_textbox=${quantity}`;
+}
+return false;
+
+
+}
+
+
+function validateQuantity(quantity) {
+    let errorMessage = "";
+
+    switch (true) {
+        case isNaN(quantity):
+            errorMessage = "Not a number. Please enter a non-negative quantity to order.";
+            break;
+        case quantity <= 0 && !Number.isInteger(quantity):
+            errorMessage = "Negatice inventory and not an Integer. Please enter a non-negative quantity to order.";
+            break;
+        case quantity <= 0:
+            errorMessage = "Negative inventory. Please enter a non-negatice quantity to order.";
+            break;
+        case !Number.isInteger(quantity):
+            errorMessage = "Not an Integer. Please enter a non-negatice quantity to order.";
+            break;
+        default:
+            errorMessage = ""; //for when no errors
+            break;
+    }
+
+    return errorMessage;
+}
