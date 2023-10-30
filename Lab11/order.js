fd9b2a7f2d4c8673b59a970b9628340949e66521
RikkiRutt <rikkir@hawaii.edu>
2023-10-29 20:27:38 -1000

    L11

--- Lab11/order.js
+++ Lab11/order.js
@@ -9,13 +9,13 @@ function validateQuantity(quantity) {
         case isNaN(quantity):
             errorMessage = "Not a number. Please enter a non-negative quantity to order.";
             break;
-        case quantity < 0 && Number.isInteger(quantity):
+        case quantity < 0 && !Number.isInteger(quantity):
             errorMessage = "Negatice inventory and not an Integer. Please enter a non-negative quantity to order.";
             break;
         case quantity <0:
             errorMessage = "Negative inventory. Please enter a non-negatice quantity to order.";
             break;
-        case Number.isInteger(quantity):
+        case !Number.isInteger(quantity):
             errorMessage = "Not an Integer. Please enter a non-negatice quantity to order.";
             break;
         default:
