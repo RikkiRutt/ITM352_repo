ec1a39cba68f26af56cfe9889c1eae0fe750c99f
RikkiRutt <rikkir@hawaii.edu>
2023-10-29 20:26:30 -1000

    L11

---
+++ Lab11/order.js
@@ -0,0 +1,27 @@
+
+
+
+
+function validateQuantity(quantity) {
+    let errorMessage = "";
+
+    switch (true) {
+        case isNaN(quantity):
+            errorMessage = "Not a number. Please enter a non-negative quantity to order.";
+            break;
+        case quantity < 0 && Number.isInteger(quantity):
+            errorMessage = "Negatice inventory and not an Integer. Please enter a non-negative quantity to order.";
+            break;
+        case quantity <0:
+            errorMessage = "Negative inventory. Please enter a non-negatice quantity to order.";
+            break;
+        case Number.isInteger(quantity):
+            errorMessage = "Not an Integer. Please enter a non-negatice quantity to order.";
+            break;
+        default:
+            errorMessage = ""; //for when no errors
+            break;
+    }
+
+    return errorMessage;
+}
