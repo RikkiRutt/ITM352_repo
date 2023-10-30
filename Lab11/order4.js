c0e564794112043efb78ec64c76f45b81ca2bd24
RikkiRutt <rikkir@hawaii.edu>
2023-10-29 23:17:59 -1000

    L11

--- Lab11/order4.js
+++ Lab11/order4.js
@@ -8,12 +8,11 @@ let validationMessage = validateQuantity(quantity);
 
 if (validationMessage!=="") {
     document.getElementById("invalidQuantity").innerHTML = validationMessage
-    window.stop();
 } else {
 
     window.location.href = `display_purchase.html?qty_textbox=${quantity}`;
-    return false;
 }
+return false;
 
 
 }
