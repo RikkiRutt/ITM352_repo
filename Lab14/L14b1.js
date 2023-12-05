a2f0d269f72d648803d68cb6349ca600d661ec48
RikkiRutt <rikkir@hawaii.edu>
2023-12-04 19:02:20 -1000

    l14

--- Lab14/L14b1.js
+++ Lab14/L14b1.js
@@ -2,8 +2,13 @@ const fs = require ('fs');
 
 let filename =__dirname+'/user_data.json';
 
-let data = fs.readFileSync(filename, 'utf-8');
 
-let user_reg_data = JSON.parse(data);
+if(fs.existsSync(filename)){
+    let data = fs.readFileSync(filename, 'utf-8');
 
-console.log(user_reg_data);
+    let user_reg_data = JSON.parse(data);
+
+    console.log(user_reg_data);
+} else {
+    console.log(`The file name ${filename} foes not exist.`);
+}
